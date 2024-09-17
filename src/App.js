451ffd854d9858d848c198398c54bc051941d608
Asish Binu Mathew <asishbm98@gmail.com>
2024-09-17 01:46:43 +0100

    OKAY

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import BingoBoard from './BingoBoard';
+import ReactGA from 'react-ga4';
 
 function App() {
+	ReactGA.initialize('G-WBTCC0H7T7');
 	return (
 		<Router>
 			<Routes>
